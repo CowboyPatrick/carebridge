909bdbb818938887360ee8b95f9bd1494723cc89
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-19 16:44:47 +0900

    finished senior signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,14 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    @user = User.new(user_params)
+    @user.caregiver = current_user
+    authorize @user
+    if @user.save
+      redirect_to dashboard_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,5 +22,11 @@ class UsersController < ApplicationController
 
   def dashboard
     authorize current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :password )
   end
 end
