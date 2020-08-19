fcfe20eae3dd87fdb6310bacdb016e0485c6e899
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-19 14:10:19 +0900

    able to login with username or email about to try and deleting email from user schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # [...]
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   include Pundit
 
   # Pundit: white-list approach.
@@ -18,5 +20,13 @@ class ApplicationController < ActionController::Base
 
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 end
