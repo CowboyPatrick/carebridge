280ceee492275184256ed93568b77847d82a96ff
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-19 12:30:32 +0900

    made some changes to make navigation a bit easier.  Saving before trying to add usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,14 @@ class User < ApplicationRecord
   has_many :buttons
   has_many :provider_actions
   has_many :seniors, class_name: "User", foreign_key: :caregiver_id
+  belongs_to :caregiver, class_name: 'User', foreign_key: :caregiver_id, optional: true
+
+  def caregiver?
+    caregiver_id.nil?
+  end
+
+  def has_seniors?
+    seniors.any?
+  end
 
 end
