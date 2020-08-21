039f5bd63a32b16a0cdc8aedb93613be4ef1d7ee
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-21 14:14:46 +0900

    added pwa support with icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
-  config.hosts << "2d66671b5e7a.ngrok.io"
+  config.hosts << "33b58d00aa3d.ngrok.io"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
