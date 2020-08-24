6e2158631afc57d064cdbab9a461f17e22fcb0f5
Jocelyn Yuan <jocelyn19950627@yahoo.com.tw>
2020-08-24 14:09:58 +0900

    this is the real commit for the memergency button

--- app/jobs/linebot_emergency_job.rb
+++ app/jobs/linebot_emergency_job.rb
@@ -1,19 +1,14 @@
 require 'line/bot'
 
-class LinebotJob < ApplicationJob
+class LinebotEmergencyJob < ApplicationJob
   queue_as :default
 
-  def perform(coordinates)
+  def perform(senior, coordinates)
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_ACCESS_TOKEN"]
     }
-    order_message = "Hello, #{order.user.caregiver.first_name.capitalize}. #{order.user.first_name.capitalize} ordered:\n"
-    order.provider_actions.each do |item|
-      order_message += item.name.capitalize  + "\n"
-    end
-
-    p order_message
+    order_message = "Hello, #{senior.caregiver.first_name.capitalize}. #{senior.first_name.capitalize} has signaled an emergency!\nhttps://www.google.com/maps/search/35.6324597,139.7013585#{coordinates}"
 
     message = {
       type: 'text',
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   end
   get '/dashboard', to: 'users#dashboard'
   post '/seniors', to: 'users#create'
-  post '/callback', to: 'api/v1/linebots#get_id'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post '/callback', to: 'linebots#get_id'
+      post '/emergency', to: 'linebots#emergency'
+    end
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
