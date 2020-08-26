eb9faddcf8efd5c415b1806353ab183c9ab86c41
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-26 13:49:37 +0900

    hardcoded in a location to get price from kroger api

--- app/services/kroger/product_search.rb
+++ app/services/kroger/product_search.rb
@@ -10,7 +10,7 @@ module Kroger
     end
 
     def execute
-      response = RestClient.get("https://api.kroger.com/v1/products?filter.term=#{@search}&filter.limit=5",
+      response = RestClient.get("https://api.kroger.com/v1/products?filter.term=#{@search}&filter.limit=5&filter.locationId=01400376",
                                 accept: "application/json",
                                 authorization: "Bearer #{Kroger::Authentication.call}")
       return JSON.parse(response)
