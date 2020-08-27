4eeb64daf0a3e077eb502744d922052ef26984b2
Jocelyn Yuan <jocelyn19950627@yahoo.com.tw>
2020-08-27 22:14:30 +0900

    arraow

--- app/javascript/components/button_selector.js
+++ app/javascript/components/button_selector.js
@@ -2,6 +2,7 @@
 const initButtonSelector = () => {  $(document).ready(function(){
     $(".category-choice").click(function(){
       $(this).toggleClass("active");
+      document.querySelector('#next-page-order i').classList.remove('d-none');
       $('.create-btn').prop("disabled", false);
     });
   });
