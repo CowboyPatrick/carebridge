89d7cc4b22578a2d17ab2957ecf05c4057aa470d
Patrick Campbell <campbellpatrickdwight@gmail.com>
2020-08-28 09:01:26 +0900

    a user logged that is loggedin will now be redirected away from the login screen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,17 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    redirect_to buttons_path if user_signed_in? && current_user.senior?
+    if user_signed_in?
+      if current_user.senior?
+        redirect_to buttons_path
+      else
+        redirect_to dashboard_path
+      end
+
+    end
+
+    # Bottom one works but why doesn't the top one?
+    # current_user.senior? ? redirect_to buttons_path : redirect_to dashboard_path
+    # redirect_to buttons_path if user_signed_in? && current_user.senior?
   end
 end
